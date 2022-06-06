34e5792ace52cfb458c3e821e27c5c05a2d1625b
Anil <786.anil.potlapally@gmail.com>
2022-06-06 19:37:19 +0530

    configuration related

---
+++ 19.Section-6/15_app_config_env_all about configuratoin.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const tourRouter = require("./13_tourRouter");
+const userRouter = require("./13_userRoute__subject_multiple_middleware");
+const morgan = require("morgan");
+
+// for loading configuration ...
+const dotenv = require("dotenv");
+
+// setting configuration path ...
+dotenv.config({ path: `${__dirname}/config.env` });
+
+const app = express();
+
+// accessing the configuration ...
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json()); // for post requrest , accessing req.body
+
+// using static files
+app.use(express.static(`${__dirname}/public/`));
+
+console.log(app.get("env"));
+console.log(process.env);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+const port = 80;
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
+
+// in terminal we can write /> SOMEVARIABLE=abc VARIABLE2=bbc nodemon app.js
+// SOMEVARIABLE=abc
+// VARIABLE2=bbc
+// are added to process.env
